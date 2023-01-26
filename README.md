595358823b71a34ce0efef3a41f62a1c672562db
Rusheb <rusheb.shah@gmail.com>
2023-01-26 07:31:21 +0000

    Make project build on Rusheb's mac

---
+++ README.md
@@ -0,0 +1,16 @@
+# tabGPT
+
+_Use GPT to classify a bunch of your browser tabs!_
+
+## Setup
+
+* Install [PyTorch](https://pytorch.org/get-started/locally/) to your global python
+* Create venv and allow it to access global packages
+
+    ```python -m venv venv --system-site-packages```
+* Activate venv
+
+    ```source venv/bin/activate```
+* Install dependencies
+
+    ```pip install -r requirements.txt```
--- classify_tabs.py
+++ classify_tabs.py
@@ -7,6 +7,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # pylint: disable=missing-class-docstring,missing-function-docstring,dangerous-default-value
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 # load model as global variable
@@ -18,7 +19,7 @@ if not typing.TYPE_CHECKING:
     # TOKENIZER = GPT2Tokenizer.from_pretrained('gpt2')
     # TOKENIZER.pad_token = TOKENIZER.eos_token
     # MODEL = GPT2LMHeadModel.from_pretrained('gpt2', pad_token_id=TOKENIZER.eos_token_id)
-    # MODEL.eval().cuda()
+    # MODEL.eval().to(device)
     # tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
     # model = AutoModelForCausalLM.from_pretrained("distilgpt2")
 
@@ -31,7 +32,7 @@ if not typing.TYPE_CHECKING:
 def generate_continuation(prompt : str, max_length : int = 5, stop_token : str|None = None) -> str:
     input_ids : torch.Tensor = TOKENIZER.encode(prompt, return_tensors="pt")
     generated_text_ids = MODEL.generate(
-        input_ids = input_ids.cuda(), 
+        input_ids = input_ids.to(device),
         max_length = max_length+len(input_ids[0]), 
         do_sample = False,
     )
@@ -46,7 +47,7 @@ def generate(prompt : str, max_length : int = 5, stop_token : str|None = None) -
 def get_logits_and_tokens(text : str):
     input_ids : torch.Tensor = TOKENIZER.encode(text, return_tensors="pt")
     tokens : list[str] = [TOKENIZER.decode([input_id]) for input_id in input_ids[0]]
-    output = MODEL(input_ids.cuda())
+    output = MODEL(input_ids.to(device))
     return output.logits[0][:-1], tokens
 
 
